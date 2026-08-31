15aeaf9e40936a518c37429359f86e4f473f4238
Paola Alejo <paola.alejo@tecsup.edu.pe>
2026-08-30 22:28:40 -0500

    Entrega final de StudentController por Responsable

--- src/StudentController.java
+++ src/StudentController.java
@@ -3,6 +3,7 @@ public class StudentController{
     public void controller(){
         // Codigo modificado por el Responsable
         // Avance en la rama sprint-1
+        // Trabajo finalizado por el responsable
     }
 
 }
